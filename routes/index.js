89a91235fb6910661f992ae8c176dbf09c53c85f
Caleb Bush <calebmatthewbush@gmail.com>
2023-02-25 23:46:51 -0600

    Send data from form post requests to message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Whaddup cuz',
+    user: 'guythat',
+    added: new Date()
+  },
+  {
+    text: 'Not much homie',
+    user: 'girlwho',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages : messages });
+  next()
 });
+
+router.get('/new', function(req, res, next){
+  res.render('form')
+})
+
+router.post('/new', function(req, res, next){
+  messages.push({ text: req.body.messageText, user: req.body.authorName, added: new Date() })
+  res.redirect('/')
+})
 
 module.exports = router;
